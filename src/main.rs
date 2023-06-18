383fc7ab3836ce502562f22096cd34b0a85d33f2
Aillet <arthur.aillet@epitech.eu>
2023-06-18 17:11:17 +0200

    feat: minimal rend-ox setup, basic window opened

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use rend_ox::app::{app, App};
+use rend_ox::nannou_egui::egui::CtxRef;
+
+pub struct Snake {
+
+}
+
+impl Snake {
+    pub fn new() -> Snake {
+        Snake{}
+    }
+}
+
+pub fn snake_update<Snake>(_: &rend_ox::nannou::App, _: &mut App<Snake>, _: rend_ox::nannou::event::Update, _: CtxRef) {
+}
+
+fn snake_app(nannou_app: &rend_ox::nannou::App) -> App<Snake> {
+    app(nannou_app, Snake::new(), snake_update)
+}
+
 fn main() {
-    println!("Hello world");
+    rend_ox::app::launch_rendox_app(snake_app);
 }
